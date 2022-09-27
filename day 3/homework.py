cc38b27d84b6c57bbaa8efe9ec2235ab41eb7d19
levani-hacker <kirola048@gmail.com>
2022-09-27 19:27:26 +0400

    first commit

---
+++ day 3/homework.py
@@ -0,0 +1,31 @@
+# str = int(input("მიუთითეთ რიცხვი:"))
+# if str > 5:
+#     print("გმადლობთ")
+# else:
+#     print("სამწუხაროდ თვენი რიცხვი 5-ზე ნაკლებია")
+
+# mylist = [11, 23, 15, 18]
+
+# def mylen(mylists):
+#     n = 0
+#     for i in mylists:
+#         n = n + 1
+        
+#     return n
+
+# y = mylen(mylist)
+
+# print(y)
+
+
+
+# for i in range(10):
+#     print(i)
+
+
+
+
+# i = 0
+# while i < 10000000000000:
+#     i += 1
+#     print(i)
